5a26942b24ad57eab59652f62b57a275f87056d9
Danoso <daniel.perezarmeria@gmail.com>
2019-10-03 20:25:24 -0500

    Nueva tabla de horarios

--- energy_management/static/js/config.js
+++ energy_management/static/js/config.js
@@ -1,2 +1,5 @@
 // API key
 const API_KEY = "pk.eyJ1IjoibHVpc2F1Y2VkYSIsImEiOiJjazByZGgwOGcwMzhjM2xsNjZ1MjBkbWFhIn0.fWK1mTXREsro3wuz3hRu2A";
+
+var selectedBuilding = null;
+var selectedMonth = null;

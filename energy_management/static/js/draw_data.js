e45fea81c7f262a3f15b4c557d89eaea1a23fc9f
Danoso <daniel.perezarmeria@gmail.com>
2019-10-06 12:41:27 -0500

    Correcciones

--- energy_management/static/js/draw_data.js
+++ energy_management/static/js/draw_data.js
@@ -1,45 +1,33 @@
-//-----------------------------------------------------------------------------------
-//Setup svg area
-//-----------------------------------------------------------------------------------
-var svgWidth = parseInt(d3.select('#grafica').style('width'));
-var svgHeight = parseInt(d3.select('#grafica').style('height'));
 
-		// Define the chart's margins as an object
-var margin= {
-			top: 30,
-			right: 30,
-			bottom: 30,
-			left: 30
-};
-
-
-// Define dimensions of the chart area
-var width = svgWidth - margin['left'] - margin['right'];
-var height = svgHeight - margin.top - margin.bottom;
-
-
-//----------------------------------------------------------------------------------
-// Create an SVG wrapper :v
-//----------------------------------------------------------------------------------
-var svg = d3.select("#grafica")
-    .append("svg")
-        .attr("height", svgHeight)
-        .attr("width", svgWidth)
+// Cuerpo del js
+if (location.pathname.includes("building2")) {
+	setBuilding(insurgentes);
+}
+else {
+	setBuilding(reforma);
+}
 
 
 
+////////////////////////////      FUNCIONES      //////////////////////////////////////////////////////////
 
-var chartGroup = svg.append("g")
-    .attr("transform", `translate(${margin['left']}, ${margin['top']})`);
+// Function used to set the selected building and redraw everything that needs to be redrawn
+function setBuilding(building) {
+	selectedBuilding = building;
+	drawGraph();
+	drawKpi();
+}
 
-
-//----------------------------------------------------------------------------------
-// JavaScript source code
-//---------------------------------------------------------------------------------
+// Function used to set the selected month and redraw everything that needs to be redrawn
+function setMonth(month) {
+	selectedMonth = month;
+	drawGraph();
+	drawKpi();
+}
 
 
 // Function used for updating x-scale var upon click on axis label :v
-function xScale(chartData){
+function xScale(chartData, width){
     //Create my xScale
     var xScale = d3.scaleBand()
         .domain(chartData.map(d => d['measurement_time(UTC)']))
@@ -51,7 +39,7 @@ function xScale(chartData){
 
 
 //Function used for updating y-scale var upon click on axis label :v
-function yScale(chartData){
+function yScale(chartData, height){
     var min = 0;
     var max =  d3.max(chartData, d=> d['power(kW)']);
     
@@ -63,26 +51,10 @@ function yScale(chartData){
 };
 
 
-
-
-
-/*
-function setBuilding(building) {
-	selectedBuilding = building;
-	redraw();
-	drawKpi();
-}
-
-function setMonth(month) {
-	selectedMonth = month;
-	redraw();
-	drawKpi();
-}
-*/
-
-
+// Function to set the Kpi's in the html
 function drawKpi() {
 	dataUrl = `/kpi/${selectedBuilding}/${selectedMonth}`;
+	console.log(dataUrl);
 	$.getJSON(dataUrl, function (data) {
 		console.log(data);
 		d3.select("#kpi").text(data.kpiTotal);
@@ -90,60 +62,76 @@ function drawKpi() {
 }
 
 
-
-/*
-// Cuerpo del js
-if (location.pathname.includes("building2")) {
-	setBuilding( insurgentes );
-}
-else {
-	setBuilding( reforma );
-}
-*/
-
 //-------------------------------------------------------------------------------------------------
 //Draw the graph
 //-------------------------------------------------------------------------------------------------
-url = `punta/${selectedBuilding}/${selectedMonth}`;
-console.log(url);
-    
-d3.json(url).then(powerData => {
-    // Cast the power measurement values to floats
-    powerData.forEach(d => {
-        d['power(kW)'] = parseFloat(d['power(kW)']);
-    });
-        
-    //--------------------------------------------------------------------------
-    //Define initial stuff for my graph :v
-    //-------------------------------------------------------------------------
-    //Create an xScale and yScale
-    var x = xScale(powerData);
-    var y = yScale(powerData);
-        
-    // Call x axis
-    var xAxis = d3.axisBottom(x);
-    
-    var xAxis = chartGroup.append('g')
-        .attr('transform', `translate(0,${height})`)
-        .call(xAxis);
-    
-    
-    //Call yAxis
-    var yAxis = d3.axisLeft(y);
-    
-    var yAxis = chartGroup.append('g')
-        .call(yAxis);
-    
-    //Draw rectangles
-    var rects = chartGroup.selectAll('rect')
-        .data(powerData);
-    
-    rects.enter()
-        .append('rect')
-            .attr('x', d => x(d['measurement_time(UTC)']))
-            .attr('y', d => y(d['power(kW)']))
-            .attr('width', x.bandwidth)
-            .attr('height', d => height - y(d['power(kW)']) )
-            .attr('fill', 'gold')
-});
+function drawGraph() {
+	url = `punta/${selectedBuilding}/${selectedMonth}`;
+	console.log(url);
 
+	//Setup svg area
+	var svgWidth = parseInt(d3.select('#grafica').style('width'));
+	var svgHeight = parseInt(d3.select('#grafica').style('height'));
+
+	// Define the chart's margins as an object
+	var margin = {
+		top: 30,
+		right: 30,
+		bottom: 30,
+		left: 30
+	};
+
+	// Define dimensions of the chart area
+	var width = svgWidth - margin['left'] - margin['right'];
+	var height = svgHeight - margin.top - margin.bottom;
+
+	// Create an SVG wrapper :v
+	var svg = d3.select("#grafica")
+		.append("svg")
+		.attr("height", svgHeight)
+		.attr("width", svgWidth)
+
+	var chartGroup = svg.append("g")
+		.attr("transform", `translate(${margin['left']}, ${margin['top']})`);
+
+	d3.json(url, function(powerData) {
+		// Cast the power measurement values to floats
+		powerData.forEach(d => {
+			d['power(kW)'] = parseFloat(d['power(kW)']);
+		});
+
+		//--------------------------------------------------------------------------
+		//Define initial stuff for my graph :v
+		//-------------------------------------------------------------------------
+		//Create an xScale and yScale
+		var x = xScale(powerData, width);
+		var y = yScale(powerData, height);
+
+		// Call x axis
+		var xAxis = d3.axisBottom(x);
+
+		var xAxis = chartGroup.append('g')
+			.attr('transform', `translate(0,${height})`)
+			.call(xAxis);
+
+
+		//Call yAxis
+		var yAxis = d3.axisLeft(y);
+
+		var yAxis = chartGroup.append('g')
+			.call(yAxis);
+
+		//Draw rectangles
+		var rects = chartGroup.selectAll('rect')
+			.data(powerData);
+
+		rects.enter()
+			.append('rect')
+			.attr('x', d => x(d['measurement_time(UTC)']))
+			.attr('y', d => y(d['power(kW)']))
+			.attr('width', x.bandwidth)
+			.attr('height', d => height - y(d['power(kW)']))
+			.attr('fill', 'gold')
+	});
+
+}
